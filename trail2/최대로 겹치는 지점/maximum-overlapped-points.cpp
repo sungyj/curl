13764a68f58693eb62022387f7e24f8e733d0c25
sungyj <146162719+sungyj@users.noreply.github.com>
2026-07-10 20:45:40 +0900

    [Passed] 최대로 겹치는 지점 (2ms, 0MB)

---
+++ trail2/최대로 겹치는 지점/maximum-overlapped-points.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int N;
+    cin>>N;
+
+    int x1,x2;
+    int visited[101]={};
+    int max_val=0;
+    for(int i=0; i<N; i++){
+        cin>>x1>>x2;
+        for(int j=x1; j<=x2; j++){
+            visited[j]++;
+            if(visited[j]>max_val) max_val=visited[j];
+        }
+    }
+    cout <<max_val;
+
+    return 0;
+}
